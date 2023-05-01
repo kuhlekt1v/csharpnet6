bd6d8a733173e35bc4698736928efa8681ad4971
Cody Sheridan <cody.sheridan@protonmail.com>
2023-05-01 07:05:34 -0400

    Initialized.

---
+++ Chapter06/PacktLibrary/Person.cs
@@ -0,0 +1,16 @@
+﻿using static System.Console;
+
+namespace PacktLibrary;
+public class Person : object
+{
+    // Fields.
+    public string? Name;
+    public DateTime DateOfBirth;
+    public List<Person> Children = new();
+
+    // Methods.
+    public void WriteToConsole()
+    {
+        WriteLine($"{Name} WaitHandleExtensions born on a {DateOfBirth:dddd}.");
+    }
+}
