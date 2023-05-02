3b64bdb8c4cc20e9be8f068a32c0e4cd2796701c
Cody Sheridan <sheridan.codyj@gmail.com>
2023-05-02 04:55:53 -0400

    Work in progress.

--- Chapter06/PacktLibrary/Person.cs
+++ Chapter06/PacktLibrary/Person.cs
@@ -3,14 +3,58 @@
 namespace PacktLibrary;
 public class Person : object
 {
-    // Fields.
-    public string? Name;
-    public DateTime DateOfBirth;
-    public List<Person> Children = new();
+  // Fields.
+  public string? Name;
+  public DateTime DateOfBirth;
+  public List<Person> Children = new();
 
-    // Methods.
-    public void WriteToConsole()
+  // Methods.
+  public void WriteToConsole()
+  {
+    WriteLine($"{Name} WaitHandleExtensions born on a {DateOfBirth:dddd}.");
+  }
+
+  // Static method to "multiply".
+  public static Person Procreate(Person p1, Person p2)
+  {
+    Person baby = new()
     {
-        WriteLine($"{Name} WaitHandleExtensions born on a {DateOfBirth:dddd}.");
+      Name = $"Baby of {p1.Name} and {p2.Name}"
+    };
+
+    p1.Children.Add(baby);
+    p2.Children.Add(baby);
+
+    return baby;
+  }
+
+  // Instance method to "multiply".
+  public Person ProcreateWith(Person partner)
+  {
+    return Procreate(this, partner);
+  }
+
+  // Operator to "multiply".
+  public static Person operator *(Person p1, Person p2)
+  {
+    return Person.Procreate(p1, p2);
+  }
+
+  // Method with a local function.
+  public static int Factorial(int number)
+  {
+    if (number < 0)
+    {
+      throw new ArgumentException(
+        $"{nameof(number)} cannot be less than zero.");
     }
+    return localFactorial(number);
+
+    // Local function.
+    int localFactorial(int localNumber)
+    {
+      if (localNumber < 1) return 1;
+      return localNumber * localFactorial(localNumber - 1);
+    }
+  }
 }
--- Chapter06/PeopleApp/Program.cs
+++ Chapter06/PeopleApp/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using PacktLibrary;
+using static System.Console;
+
+Person harry = new() { Name = "Harry" };
+Person mary = new() { Name = "Mary" };
+Person jill = new() { Name = "Jill" };
+
+// Call instance method.
+Person baby1 = mary.ProcreateWith(harry);
+baby1.Name = "Gary";
+
+// Call static method.
+Person baby2 = Person.Procreate(harry, jill);
+
+// Call an operator.
+Person baby3 = harry * mary;
+
+
+WriteLine($"{harry.Name} has {harry.Children.Count} children.");
+WriteLine($"{mary.Name} has {mary.Children.Count} children.");
+WriteLine($"{jill.Name} has {jill.Children.Count} children.");
+WriteLine(
+    format: "{0}'s first child is named \"{1}\".",
+    arg0: harry.Name,
+    arg1: harry.Children[0].Name
+);
+
+WriteLine("");
+WriteLine($"5! is {Person.Factorial(5)}");
+
