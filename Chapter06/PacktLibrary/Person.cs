04ead8fe8a49be765e677a28af54b38301c9c1be
Cody Sheridan <cody.sheridan@protonmail.com>
2023-05-02 05:19:48 -0400

    Defining & handling delegates.

--- Chapter06/PacktLibrary/Person.cs
+++ Chapter06/PacktLibrary/Person.cs
@@ -3,58 +3,80 @@
 namespace PacktLibrary;
 public class Person : object
 {
-  // Fields.
-  public string? Name;
-  public DateTime DateOfBirth;
-  public List<Person> Children = new();
+    // Fields.
+    public string? Name;
+    public DateTime DateOfBirth;
+    public List<Person> Children = new();
 
-  // Methods.
-  public void WriteToConsole()
-  {
-    WriteLine($"{Name} WaitHandleExtensions born on a {DateOfBirth:dddd}.");
-  }
-
-  // Static method to "multiply".
-  public static Person Procreate(Person p1, Person p2)
-  {
-    Person baby = new()
+    // Methods.
+    public void WriteToConsole()
     {
-      Name = $"Baby of {p1.Name} and {p2.Name}"
-    };
-
-    p1.Children.Add(baby);
-    p2.Children.Add(baby);
-
-    return baby;
-  }
-
-  // Instance method to "multiply".
-  public Person ProcreateWith(Person partner)
-  {
-    return Procreate(this, partner);
-  }
-
-  // Operator to "multiply".
-  public static Person operator *(Person p1, Person p2)
-  {
-    return Person.Procreate(p1, p2);
-  }
-
-  // Method with a local function.
-  public static int Factorial(int number)
-  {
-    if (number < 0)
-    {
-      throw new ArgumentException(
-        $"{nameof(number)} cannot be less than zero.");
+        WriteLine($"{Name} WaitHandleExtensions born on a {DateOfBirth:dddd}.");
     }
-    return localFactorial(number);
 
-    // Local function.
-    int localFactorial(int localNumber)
+    // Static method to "multiply".
+    public static Person Procreate(Person p1, Person p2)
     {
-      if (localNumber < 1) return 1;
-      return localNumber * localFactorial(localNumber - 1);
+        Person baby = new()
+        {
+            Name = $"Baby of {p1.Name} and {p2.Name}"
+        };
+
+        p1.Children.Add(baby);
+        p2.Children.Add(baby);
+
+        return baby;
     }
-  }
+
+    // Instance method to "multiply".
+    public Person ProcreateWith(Person partner)
+    {
+        return Procreate(this, partner);
+    }
+
+    // Operator to "multiply".
+    public static Person operator *(Person p1, Person p2)
+    {
+        return Person.Procreate(p1, p2);
+    }
+
+    // Method with a local function.
+    public static int Factorial(int number)
+    {
+        if (number < 0)
+        {
+            throw new ArgumentException(
+              $"{nameof(number)} cannot be less than zero.");
+        }
+        return localFactorial(number);
+
+        // Local function.
+        int localFactorial(int localNumber)
+        {
+            if (localNumber < 1) return 1;
+            return localNumber * localFactorial(localNumber - 1);
+        }
+    }
+
+    // Delegate field.
+    public EventHandler? Shout;
+
+    // Data field.
+    public int AngerLevel;
+
+    // Method.
+    public void Poke()
+    {
+        AngerLevel++;
+
+        if (AngerLevel >= 3)
+        {
+            // If something is listening...
+            if (Shout != null)
+            {
+                // ... then call the delegate.
+                Shout(this, EventArgs.Empty);
+            }
+        }
+    }
 }
--- Chapter06/PeopleApp/Program.cs
+++ Chapter06/PeopleApp/Program.cs
@@ -28,3 +28,17 @@ WriteLine(
 WriteLine("");
 WriteLine($"5! is {Person.Factorial(5)}");
 
+static void Harry_Shout(object? sender, EventArgs e)
+{
+    if (sender is null) return;
+    Person p = (Person)sender;
+    WriteLine($"{p.Name} is this angry; {p.AngerLevel}.");
+}
+
+WriteLine("");
+harry.Shout = Harry_Shout;
+harry.Poke();
+harry.Poke();
+harry.Poke();
+harry.Poke();
+
